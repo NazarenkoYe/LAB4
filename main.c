855d01cf9bcdd963c9082ca6ae2c6b3bcdcf7e31
NazarenkoYe <egornazarenko11@gmail.com>
2024-07-09 19:01:40 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int count_equal_divisors(int n) {
+int countequaldivisors(int n) {
     int count = 0;
     for (int m = 1; m < n; m++) {
         if (n / m == n % m) {
@@ -11,13 +11,13 @@ int count_equal_divisors(int n) {
 }
 int main() {
     int n;
-    printf("Enter a natural number n (1 < n < 150): ");
+    printf("Enter a natural number n: ");
     scanf("%d", &n);
     if (n <= 1 || n >= 150) {
-        printf("Invalid input. The value of n must be between 2 and 149.\n");
+        printf("Invalid input.\n");
         return 1;
     }
-    int result = count_equal_divisors(n);
+    int result = countequaldivisors(n);
     printf("The number of equal divisors of %d is: %d\n", n, result);
     return 0;
 }
